7863e24c70f5df7f0e67ffcfb7df08012adce43e
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-03 07:32:44 +0000

    refactored the code and did unit testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,17 @@
 const request = require('request');
-const breedName = process.argv[2];
-//checking if the argument is being passed
-if (!breedName) {
-  console.log('Breed name hasn\'t been entered!');
-  process.exit();
-}
-let url = "https://api.thecatapi.com/v1/breeds/search?name=" + breedName;
-
-request(url,(error, response, body) => {
-  if (error) {
-    console.log(response && response.statusCode);
-    console.log(error);
-  } else {
+//function to return description of the breed
+const fetchBreedDescription = function(breedName, callback) {
+  //checking if the argument is being passed
+  let url = "https://api.thecatapi.com/v1/breeds/search?name=" + breedName;
+  request(url,(error, response, body) => {
     const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+    try {
+      callback(error, data[0].description);
+    } catch (err) {
+      callback(err);
+    }
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
